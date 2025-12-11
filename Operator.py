ec255abb34b603d440c8edbf960818c58a5716ee
pilipine <philippinepelletratdeborde@gmail.com>
2025-12-11 10:16:45 +0100

    11/12/2025

--- Operator.py
+++ Operator.py
@@ -1,6 +1,5 @@
 # Import de la classe Member
-from Member import *  # ou: from classes.member import Member
-
+from Member import * 
 
 class Operator(Member):
     def __init__(self, first_name, last_name, gender, age, role, experience=0):
@@ -23,7 +22,24 @@ class Operator(Member):
         self.__experience = int(new_experience)
 
     # Définition d'une méthode qui dit ce que fait l'Operator selon son role
+
     def act(self):
+        full_name = f"{self.get_first_name()} {self.get_last_name()}"
+        r = (self.role or "")
+
+        if r in ("pilot", "pilote"):
+            print(f"{full_name} pilote le vaisseau")
+        elif r in ("technician", "technicien", "technicienne"):
+            print(f"{full_name} répare le vaisseau")
+        else:
+            print(f"{full_name} ne fait rien")
+
+#class role(Operator):
+    #def __init__(self, role, gain_experience):
+        super().__init__(role, gain_experience)
+        self.__role= role
+
+    #def act(self):
         if self.get_role() == "pilot":
             print(f"{self.get_first_name()} {self.get_last_name()} pilote le vaisseau")
         if self.get_role().lower() == "technician":
@@ -31,5 +47,5 @@ class Operator(Member):
         else:
             print("Ne fait rien")
 
-    def gain_experience(self):
+    #def gain_experience(self):
         self.__experience = self.__experience + 1
--- main.py
+++ main.py
@@ -3,12 +3,19 @@ from Operator import *
 from Mentalist import *
 from Spaceship import *
 
-# Attribut supplémentaire sera : le rôle et l'expérience
-# Méthodes supplémentaires : act(), gain_experience(), getters/setters.
 
-action = {
-    "pilote": "piloter le vaisseau",
-    "technicien": "réparer le vaisseau en cas d'attaque",
-    "marchand": "échanger ou vendre des produits",
-    "soldat": "combattre les ennemies en cas d'attaque",
-}
+# main.py
+
+if __name__ == "__main__":
+    op1 = Operator("Bel", "Riose", "Homme", 48, "Pilote")
+    op1.act()  # -> "Gaal Dornick répare le vaisseau"
+    op1.gain_experience()
+    print(op1.get_experience())  # -> 1
+
+    op2 = Operator("Salvor", "Hardin", "F", 27, "pilote", experience=2)
+    op2.act()  # -> "Salvor Hardin pilote le vaisseau"
+    op2.gain_experience(3)
+    print(op2.get_experience())  # -> 5
+
+    op3 = Operator("Hober", "Mallow", "M", 30, "marchand")
+    op3.act()  # -> "Hober Mallow ne fait rien"
