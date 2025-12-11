5ff3d4486d33f14c1287d715dde9190ff8ae4fd1
pilipine <philippinepelletratdeborde@gmail.com>
2025-12-11 11:19:41 +0100

    11/12/2025

--- Operator.py
+++ Operator.py
@@ -1,5 +1,6 @@
 # Import de la classe Member
-from Member import * 
+from Member import *
+
 
 class Operator(Member):
     def __init__(self, first_name, last_name, gender, age, role, experience=0):
@@ -25,7 +26,7 @@ class Operator(Member):
 
     def act(self):
         full_name = f"{self.get_first_name()} {self.get_last_name()}"
-        r = (self.role or "")
+        r = self.role or ""
 
         if r in ("pilot", "pilote"):
             print(f"{full_name} pilote le vaisseau")
@@ -34,12 +35,12 @@ class Operator(Member):
         else:
             print(f"{full_name} ne fait rien")
 
-#class role(Operator):
-    #def __init__(self, role, gain_experience):
+        # class role(Operator):
+        # def __init__(self, role, gain_experience):
         super().__init__(role, gain_experience)
-        self.__role= role
+        self.__role = role
 
-    #def act(self):
+        # def act(self):
         if self.get_role() == "pilot":
             print(f"{self.get_first_name()} {self.get_last_name()} pilote le vaisseau")
         if self.get_role().lower() == "technician":
@@ -47,5 +48,5 @@ class Operator(Member):
         else:
             print("Ne fait rien")
 
-    #def gain_experience(self):
+        # def gain_experience(self):
         self.__experience = self.__experience + 1
--- main.py
+++ main.py
@@ -6,16 +6,7 @@ from Spaceship import *
 
 # main.py
 
-if __name__ == "__main__":
-    op1 = Operator("Bel", "Riose", "Homme", 48, "Pilote")
-    op1.act()  # -> "Gaal Dornick répare le vaisseau"
-    op1.gain_experience()
-    print(op1.get_experience())  # -> 1
-
-    op2 = Operator("Salvor", "Hardin", "F", 27, "pilote", experience=2)
-    op2.act()  # -> "Salvor Hardin pilote le vaisseau"
-    op2.gain_experience(3)
-    print(op2.get_experience())  # -> 5
-
-    op3 = Operator("Hober", "Mallow", "M", 30, "marchand")
-    op3.act()  # -> "Hober Mallow ne fait rien"
+# Instancier un nouveau membre
+Bel_riose = Member("Bel", "Riose", "Homme", 48)
+Bel_riose.introduce_yourself()
+act(Bel_rioseS)
